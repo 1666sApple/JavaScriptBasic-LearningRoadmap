1e02b9efcb953012a5fc1fc1aef79de19c5f96a7
1666sApple <ananno.034@gmail.com>
2024-07-27 04:41:12 +0600

    Add Basics

---
+++ 01.basics/index.js
@@ -0,0 +1,27 @@
+// Print a message to the console
+console.log("hello world");
+
+// Print another message to the console
+console.log("I am learning javascript");
+
+// Display a window alert with a message
+// window.alert("Alert Generated! Press ok to proceed");
+
+// Set the text content of the element with id "header"
+document.getElementById("header").textContent = "Hello World";
+
+// Set the text content color of the element with id "header" to red
+document.getElementById("header").style.color = "red";
+
+// Set the text content background of the element with id "header" to grey
+// document.getElementById("header").style.backgroundColor = "grey";
+
+// Set the text content of the element with id "para"
+document.getElementById("para").textContent = "Learning Javascript";
+
+// Set the text content color of the element with id "para" to blue
+document.getElementById("para").style.color = "blue";
+
+// Variable declaration in javascript 
+
+let x;
