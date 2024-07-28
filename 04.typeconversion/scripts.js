d30d134ed374986c518d6da3dd97044969e4476e
1666sApple <ananno.034@gmail.com>
2024-07-29 01:44:46 +0600

    Add TypeConversion

--- 04.typeconversion/scripts.js
+++ 04.typeconversion/scripts.js
@@ -1,1 +1,20 @@
 document.getElementById("header").style.color = "darkgreen";
+document.getElementById("label").textContent = "Enter your Birth year: ";
+
+function updateYear() {
+  let year = document.getElementById("year").value;
+  if (year && !isNaN(year)) {
+    const currentYear = newDate().getFullYear();
+    const age = currentYear - parseInt(year, 10); // Conversion of string to integer with base 10
+    document.getElementById("header").textContent = `You are ${age} years old`;
+  } else {
+    document.getElementById("header").textContent = "Please enter a valid year";
+  }
+}
+
+document.getElementById("sub").onclick = updateYear;
+document.getElementById("year").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    updateYear();
+  }
+});
