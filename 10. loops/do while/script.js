1fbbd9410fbab60c40523ef974c3266d260eb091
1666sApple <ananno.034@gmail.com>
2024-08-16 02:23:26 +0600

    Modify Do While and While Loop

--- 10. loops/do while/script.js
+++ 10. loops/do while/script.js
@@ -1,6 +1,14 @@
+let loggedIn = false;
 let username;
+let password;
 
 do {
   username = prompt("Enter your username: ");
-} while (username === "" || username === null);
-console.log(`Hello ${username}`);
+  password = prompt("Enter your password: ");
+  if (username !== "" && password !== "") {
+    loggedIn = true;
+    console.log("You are logged in!");
+  } else {
+    console.log("Invalid username or password. Please try again.");
+  }
+} while (!loggedIn);
--- 10. loops/while loop/script.js
+++ 10. loops/while loop/script.js
@@ -1,6 +1,14 @@
-let username = "";
+let loggedIn = false;
+let username;
+let password;
 
-while (username === "" || username === null) {
+while (!loggedIn) {
   username = prompt("Enter your username: ");
+  password = prompt("Enter your password: ");
+  if (username !== "" && password !== "") {
+    loggedIn = true;
+    console.log("You are logged in!");
+  } else {
+    console.log("Invalid username or password. Please try again.");
+  }
 }
-console.log(`Hello ${username}`);
